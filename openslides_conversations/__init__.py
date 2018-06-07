9936584244077fc2282dac0a1e2af852ffa3f004
Sean Engelhardt <sean.f.t.engelhardt@gmail.com>
2018-06-07 15:46:06 +0200

    Update to OpenSlides 2.2

--- openslides_conversations/__init__.py
+++ openslides_conversations/__init__.py
@@ -1,6 +1,6 @@
 __verbose_name__ = 'OpenSlides Conversations'
 __description__ = 'Enables the usage of WebRTC Features in Openslides (WIP)'
-__version__ = '0.1 (BETA)'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 __url__ = 'https://openslides.org'
 
